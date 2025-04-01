39f9ff6708e5f3795cc4e6dedaa2aecc18619c0c
benjaminv <benvee@gmail.com>
2025-04-01 16:50:29 +1100

    setup: bump up, init Swiper globally

--- src/frontend/entrypoints/app.js
+++ src/frontend/entrypoints/app.js
@@ -1,6 +1,12 @@
 import 'vite/modulepreload-polyfill';
 import Alpine from 'alpinejs';
+import Swiper from 'swiper/bundle';
+// import styles bundle
+import 'swiper/css/bundle';
 
+window.Swiper = Swiper;
 window.Alpine = Alpine;
+
+window.Alpine.start();
 
 Alpine.start();
